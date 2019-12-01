bd27898f38de9c0c51b3eb89115384808a593059
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2019-12-01 14:58:09 -0500

    re-install node-modules and icons. sidebar and content work. index.html work.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Header } from './components/layout/Header';
+import { Content } from './components/layout/Content';
 
 
 export const App = () => (
     <div>
-      <h1>hi there</h1>
+      <Header />
+      <Content />
     </div>
 )
 
