6fe9fb1cabc3666e617b2aaaebccaaa334f84d39
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2019-12-02 01:49:42 -0500

    working on scss

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -9,17 +9,17 @@ export const Sidebar = () => (
     <div className="sidebar" data-testid="sidebar">
         <ul className="sidebar__generic">
 
-            <li>
+            <li data-testid="inbox" className="inbox">
                 <span><FaInbox /></span>
-                <span>Inbox</span>
+                <span>| Inbox</span>
             </li>
-            <li>
+            <li data-testid="today" className="today">
                 <span><FaRegCalendar /></span>
-                <span>Today</span>
+                <span>| Today</span>
             </li>
-            <li>
+            <li data-testid="next_7" className="next_7">
                 <span><FaRegCalendarAlt /></span>
-                <span>Next 7 days</span>
+                <span>| Next 7 days</span>
             </li>
         </ul>
         <div className="siderbar__middle">
